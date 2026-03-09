09af8c0beae413082dd21608d68c3067c67053c4
Filipe <filipefranciscof3@gmail.com>
2026-03-08 22:37:36 -0300

    DocGen update

--- src/UserService.php
+++ src/UserService.php
@@ -1,18 +1,8 @@
 <?php
 
-/**
- * Classe responsável pela gestão de usuários, incluindo criação e busca de usuários por ID.
- */
 class UserService {
 
 
-        /**
-         * Cria um novo usuário com um ID aleatório, nome e email fornecidos.
-         * 
-         * @param mixed $name O nome do usuário a ser criado.
-         * @param mixed $email O email do usuário a ser criado.
-         * @return mixed Um array contendo o ID, nome e email do usuário.
-         */
     public function createUser($name, $email) {
         return [
             "id" => rand(1, 1000),
@@ -21,13 +11,7 @@ class UserService {
         ];
     }
 
- 
-        /**
-         * Retorna um array com informações de um usuário com base no ID fornecido.
-         * 
-         * @param mixed $id O identificador do usuário a ser encontrado.
-         * @return mixed Um array contendo o ID, nome e email do usuário.
-         */
+
     public function findUserById($id) {
         return [
             "id" => $id,
