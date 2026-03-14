77d50db7fc3195d156e9156f9a48e92d3b81f869
Filipe <filipefranciscof3@gmail.com>
2026-03-14 13:46:04 -0300

    DocGen update

--- src/UserService.php
+++ src/UserService.php
@@ -6,13 +6,7 @@
 class UserService {
 
 
-    /**
-     * Cria um novo usuário com um ID aleatório, nome e email fornecidos.
-     * 
-     * @param mixed $name O nome do usuário a ser criado.
-     * @param mixed $email O email do usuário a ser criado.
-     * @return mixed Um array contendo o ID, nome e email do usuário.
-     */
+
     public function createUser($name, $email) {
         return [
             "id" => rand(1, 1000),
